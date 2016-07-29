5dbc68c5cc300d97b4a1f4ed7349e884bcf41235
dospunk <lostboycmd@gmail.com>
2016-07-28 21:14:26 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const sio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = sio(server);
+
+app.get('/', function(req, res){
+  res.sendFile('./home.html', {root: __dirname});
+});
+
+
+io.on('connection', function(socket){
+	console.log("a user connected with ID " + socket.id);
+	socket.on('msg', function(msg){
+		io.emit('msg', name + ": " + msg.message);
+	});
+	socket.on('set-name', function(nickname){
+		name = nickname;
+	});
+	io.on('disconnect', function(){
+		console.log("user " + socket.id + " disconnected");
+	});
+});
+
+server.listen(5000, function(){
+  console.log('listening on *:5000');
+});
---
+++ start_server.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\user\Desktop\Chat
+start nodemon index.js
